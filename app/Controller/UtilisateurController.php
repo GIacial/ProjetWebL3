3942e59c6119ec33e2708819dcdd58eef55daa65
GIacial <cristalsaga@hotmail.fr>
2017-03-08 15:34:11 +0100

    User Controller

---
+++ app/Controller/UtilisateurController.php
@@ -0,0 +1,24 @@
+<?php
+
+class ProduitController extends AppController {
+
+    public $uses = array('Utilisateur');      //Model a utiliser
+    public $viewPath = 'UtilisateurView';    //le nom du sous repertoire des vue du controlleur
+
+    public function index(){
+    	$this->redirect(array('action' => 'connexion'));
+    }
+
+    public function connexion(){
+    	if($this->request->is('post')){
+    		if($this->Auth->login()){
+    			$this->redirect($this->Auth->redirectUrl())
+    		}
+    		$this->Flash->error('Impossible de vous identifier');
+    	}
+    }
+}
+
+
+
+?>
--- app/View/produitView/produit.ctp
+++ app/View/produitView/produit.ctp
@@ -1,5 +1,21 @@
-<p>
+<div>
 
-<h1> TABLEAU </h1>
-<?php pr($tab); ?>
-</p>
+	<?php
+	  
+	 echo $this->Form->create('Panier');
+	 for( $i = 0 ; $i< count($tab) ; $i++){
+	 	echo '<div>';
+	 	echo '<p>'.$tab[$i]['Produit']['libelle'].'</p>';
+	 	echo $this->Form->number('nombre',array(
+	 											'min' => '0',
+	 											'default' => '0',
+	 											'max' => $tab[$i]['Produit']['stock'],
+	 											));
+	 	echo '</div>';
+	 }
+	 echo $this->Form->end('Ajouter au panier');
+
+
+	 echo debug($tab);
+	?>
+</div>
